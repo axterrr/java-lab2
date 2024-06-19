a1aef30e4224efb784ee7fa15b9745e91658ec39
axterrr <vgibskiy@gmail.com>
2024-06-19 04:31:44 +0300

    skip-tests profile

---
+++ converter-core/src/test/java/org/hibskyi/CurrencyConverterTest.java
@@ -0,0 +1,18 @@
+package org.hibskyi;
+
+import junit.framework.TestCase;
+
+import java.util.HashMap;
+
+public class CurrencyConverterTest extends TestCase {
+    public void testCurrencyConverter() {
+        HashMap<String, Double> map = new HashMap<>();
+        map.put("UAH", 1.0);
+        map.put("USD", 40.0);
+        map.put("EUR", 45.0);
+        CurrencyConverter converter = new CurrencyConverter(map);
+        assertEquals(converter.convert("UAH", 5), 5.0);
+        assertEquals(converter.convert("USD", 10), 400.0);
+        assertEquals(converter.convert("EUR", 1), 45.0);
+    }
+}
